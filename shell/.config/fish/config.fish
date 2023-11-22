bdc168744281d9801c593ee8f8a7a7d8e568ee9e
Edward Källstedt <edward.kallstedt@gmail.com>
2023-11-22 13:30:27 +0100

    add fish config

---
+++ shell/.config/fish/config.fish
@@ -0,0 +1,55 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -U fish_greeting
+fish_add_path $HOME/.cargo/bin
+fish_add_path $HOME/.local/bin
+
+# pnpm
+set -gx PNPM_HOME $HOME/.local/share/pnpm
+if not string match -q -- $PNPM_HOME $PATH
+  set -gx PATH "$PNPM_HOME" $PATH
+end
+# pnpm end
+
+# application env
+set -gx EDITOR nvim
+
+
+# Fuzzy find a folder, if there is a tmux session in folder, attach to it,
+# otherwise create a new session with the folder as the working directory
+function fzf_cd_tmux
+    set selected_dir (fd --max-depth 2 --type directory --full-path . $HOME | fzf)
+
+    if test -n "$selected_dir"
+        set session_name (basename "$selected_dir")
+
+        if tmux has-session -t $session_name 2>/dev/null
+            tmux attach -t $session_name
+        else
+            tmux new-session -s $session_name -c "$selected_dir"
+        end
+    else
+        echo "No directory selected."
+    end
+end
+
+# Custom Shortcuts
+bind \ck fzf_cd_tmux 
+
+# Aliases
+alias ipi="curl ipinfo.io"
+alias b="acpi -b"
+alias screenshot="~/.scripts/screenshot.sh"
+alias screenshot_region="~/.scripts/screenshot_region.sh"
+alias ltu="cd ~/Documents/ltu"
+alias vs='source .venv/bin/activate.fish'
+alias clearpac='pacman -Rns $(pacman -Qtdq)'
+alias cz='cd && cd "$(dirname "$(fzf)")"'
+alias t='cd $(cat ~/.config/cur_dir)'
+alias ts='pwd > ~/.config/cur_dir'
+alias l='ls -lah'
+
+#this should be at the end of the file
+zoxide init fish | source
---
+++ shell/.config/fish/functions/br.fish
@@ -0,0 +1,1 @@
+/home/edward/.local/share/broot/launcher/fish/br.fish

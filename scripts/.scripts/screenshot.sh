f6a492289342d20ade0c155257e148f2145e8254
Edward Källstedt <edward.kallstedt@gmail.com>
2023-12-01 15:08:27 +0100

    add notify to screenshot script

--- scripts/.scripts/screenshot.sh
+++ scripts/.scripts/screenshot.sh
@@ -1,7 +1,8 @@
 #Screenshot entire screen
-screen_dir=$HOME/pictures/screenshots
+screen_dir=$HOME/Pictures/screenshots
 if [ -d $screen_dir ]
 then
 	timestamp=$(date +"%y-%m-%d_%T")
         grim "$screen_dir/$timestamp.png"
+        notify-send "Screenshot taken"
 fi

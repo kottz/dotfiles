b132e8aa31d21a52e6d5716b3561847dbb66c52f
Edward Källstedt <edward.kallstedt@gmail.com>
2025-08-16 02:26:12 +0000

    nvim: fix copy/paste over ssh in tmux

--- editor/.config/nvim/init.lua
+++ editor/.config/nvim/init.lua
@@ -25,7 +25,34 @@ vim.o.showmode = false
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-  vim.o.clipboard = 'unnamedplus' -- Kickstart's default, your old config had this commented
+  --vim.o.clipboard = 'unnamedplus' -- Kickstart's default, your old config had this commented
+  vim.g.clipboard = {
+    name = 'OSC 52',
+    copy = {
+      ['+'] = require('vim.ui.clipboard.osc52').copy '+',
+      ['*'] = require('vim.ui.clipboard.osc52').copy '*',
+    },
+    paste = {
+      ['+'] = require('vim.ui.clipboard.osc52').paste '+',
+      ['*'] = require('vim.ui.clipboard.osc52').paste '*',
+    },
+  }
+  if vim.env.TMUX ~= nil then
+    local copy = { 'tmux', 'load-buffer', '-w', '-' }
+    local paste = { 'bash', '-c', 'tmux refresh-client -l && sleep 0.05 && tmux save-buffer -' }
+    vim.g.clipboard = {
+      name = 'tmux',
+      copy = {
+        ['+'] = copy,
+        ['*'] = copy,
+      },
+      paste = {
+        ['+'] = paste,
+        ['*'] = paste,
+      },
+      cache_enabled = 0,
+    }
+  end
 end)
 
 -- Enable break indent
